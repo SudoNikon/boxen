e7a8221b6186e1495add9b62b5523914a9d2780c
Ben Olive <ben.olive@salesloft.com>
2014-03-11 09:25:50 -0400

    Add htop, heroku-toolbelt, and googledrive to my manifest

--- modules/people/manifests/sionide21.pp
+++ modules/people/manifests/sionide21.pp
@@ -4,13 +4,19 @@ class people::sionide21 {
   include chrome
   include dropbox
   include gitx
+  include googledrive
   include hipchat
   include iterm2::dev
   include textmate
 
+  # Homebrew bash completion
   file { "$homebrew::config::installdir/etc/bash_completion.d/brew":
     ensure => "$homebrew::config::installdir/Library/Contributions/brew_bash_completion.sh",
   }
+
+  package { 'htop-osx': }
+
+  package { 'heroku-toolbelt': }
 
   include projects::all
 }
