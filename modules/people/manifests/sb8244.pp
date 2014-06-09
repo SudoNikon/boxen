bc7e428ce7941a391e2c279fd0c3b6c5b10f7f0a
Stephen Bussey <stephenbussey@Stephens-MacBook-Pro.local>
2014-06-09 15:24:28 -0400

    Add sb8244 manifest

---
+++ modules/people/manifests/sb8244.pp
@@ -0,0 +1,22 @@
+class people::sb8244 {
+  include bash
+  include bash::completion
+  include chrome
+  include dropbox
+  include gitx
+  include googledrive
+  include hipchat
+  include iterm2::dev
+  include textmate
+
+  # Homebrew bash completion
+  file { "$homebrew::config::installdir/etc/bash_completion.d/brew":
+    ensure => "$homebrew::config::installdir/Library/Contributions/brew_bash_completion.sh",
+  }
+
+  package { 'htop-osx': }
+
+  package { 'heroku-toolbelt': }
+
+  include projects::all
+}
