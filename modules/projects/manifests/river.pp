950ee98c4cd8b2bf48af8b5cf83081dcc62c5c58
Ben Olive <ben.olive@salesloft.com>
2014-06-18 13:55:09 -0400

    Udate river ruby

--- modules/projects/manifests/river.pp
+++ modules/projects/manifests/river.pp
@@ -3,5 +3,6 @@ class projects::river {
     # For now I am not committing this file, you can add your own to projects/files/... if you want to
     # application_yml => 'puppet:///modules/projects/river/application.yml',
     port            => 9292,
+    ruby            => '2.1.2',
   }
 }
