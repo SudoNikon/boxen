04938e96d0625257644035fc7a7ed724df1dc1fb
Ben Olive <ben.olive@salesloft.com>
2014-06-20 15:31:01 -0400

    Include chrome-cli with bower

--- modules/bower/manifests/init.pp
+++ modules/bower/manifests/init.pp
@@ -2,6 +2,8 @@ class bower {
   include nodejs::v0_10
   class { 'nodejs::global': version => 'v0.10' }
 
+  package { 'chrome-cli': }
+
   nodejs::module { 'bower':
     node_version => 'v0.10',
     ensure => '1.2.8',
--- modules/projects/manifests/prospectorjs.pp
+++ modules/projects/manifests/prospectorjs.pp
@@ -3,9 +3,6 @@ class projects::prospectorjs {
   include nodejs::v0_10
   include bower
 
-  # Waiting on https://github.com/Homebrew/homebrew/pull/27077
-  # package { 'chrome-cli': }
-
   boxen::project { 'prospectorjs':
     source => 'SalesLoft/prospectorjs'
   }
