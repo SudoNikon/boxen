83b6fa23f20399c100c91c9fe4174a1951556588
Stephen Bussey <stephenbussey@Stephens-MacBook-Pro.local>
2014-08-04 16:33:15 -0400

    Default ruby

--- modules/railsapp/manifests/init.pp
+++ modules/railsapp/manifests/init.pp
@@ -1,7 +1,7 @@
 define railsapp (
   $application_yml = undef,
   $port            = undef,
-  $ruby            = '2.1.1',
+  $ruby            = '2.1.2',
   $mongodb         = undef,
 ) {
   include boxen::config
