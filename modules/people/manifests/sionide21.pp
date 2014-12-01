d8630ed4c188fe0381e5b269497d607df7d2d419
Ben Olive <ben.olive@salesloft.com>
2014-12-01 15:03:31 -0500

    Add settings I never got around to boxenifying

--- modules/people/manifests/sionide21.pp
+++ modules/people/manifests/sionide21.pp
@@ -9,6 +9,27 @@ class people::sionide21 {
   include iterm2::dev
   include textmate
 
+  # OS Settings
+  include osx::global::enable_standard_function_keys
+  include osx::global::tap_to_click
+  include osx::dock::autohide
+
+  # Save Screenshots to a folder on the desktop
+  file { "/Users/${::boxen_user}/Desktop/ScreenShots/":
+    ensure => "directory",
+  }
+  boxen::osx_defaults { 'Save Screenshots to Folder':
+    user    => $::boxen_user,
+    key     => 'location',
+    domain  => 'com.apple.screencapture',
+    value   => "/Users/${::boxen_user}/Desktop/ScreenShots/",
+    notify  => Exec['killall SystemUIServer'],
+    require => File["/Users/${::boxen_user}/Desktop/ScreenShots/"],
+  }
+  exec { 'killall SystemUIServer':
+    refreshonly => true,
+  }
+
   # Homebrew bash completion
   file { "$homebrew::config::installdir/etc/bash_completion.d/brew":
     ensure => "$homebrew::config::installdir/Library/Contributions/brew_bash_completion.sh",
