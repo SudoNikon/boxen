10acb13a30209a764b1272af9a6db9e590b562a4
Ben Olive <ben.olive@salesloft.com>
2015-06-19 16:05:15 -0400

    Trust our local cert in ruby!
    Nasty hacks: Run a shell script that checks every installed ruby keystore
    for our cert and adds it if needed. Execute this on new cert generation
    and on any ruby installation

--- modules/ssl/manifests/cert.pp
+++ modules/ssl/manifests/cert.pp
@@ -4,12 +4,24 @@ define ssl::cert() {
   exec { "Generate certificate ${title}":
     command => "openssl req -x509 -newkey rsa:2048 -nodes -keyout ${certpath}.key -out ${certpath}.crt -days 1024 -subj '/C=US/ST=Georgia/O=SalesLoft/localityName=Atlanta/commonName=${title}'",
     creates => "${certpath}.crt",
-    notify  => Exec["Add trusted certificate ${title}"],
   }
 
   exec { "Add trusted certificate ${title}":
     refreshonly => true,
     command     => "security add-trusted-cert -d -r trustRoot -k '/Library/Keychains/System.keychain' ${certpath}.crt",
     user        => root,
+    subscribe   => Exec["Generate certificate ${title}"]
   }
+
+  # Looks through all the ruby versions and install this certificate
+  # to each keystore if not already present
+  exec { "Add trusted certificate ${title} to rubies":
+    refreshonly => true,
+    command     => template("ssl/cert/install_rubies.sh.erb"),
+    user        => root,
+    provider    => "shell",
+    subscribe   => Exec["Generate certificate ${title}"],
+  }
+  # Re-run this if a new ruby version is installed
+  Ruby::Version <| |> ~> Exec["Add trusted certificate ${title} to rubies"]
 }
