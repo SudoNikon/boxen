ffc545ff5202afb435e692c8b07e87d2f0dfecee
Justin Barber <barber.justin@gmail.com>
2015-09-23 12:55:35 -0400

    fix ruby versions for crm_repo

--- modules/projects/manifests/crm_repo.pp
+++ modules/projects/manifests/crm_repo.pp
@@ -3,6 +3,6 @@ class projects::crm_repo {
     # For now I am not committing this file, you can add your own to projects/files/... if you want to
     # application_yml => 'puppet:///modules/projects/crm_repo/application.yml',
     port => 9191,
-    ruby => '2.1.2',
+    ruby => '2.2.2'
   }
 }
