9d5f8b9f5c229379396c99e17c9d95ffb8e97359
Patrick Wiseman <patrick.wiseman@salesloft.com>
2015-09-29 10:06:36 -0400

    Manifest for Daniel

---
+++ modules/people/manifests/danielandrews.pp
@@ -0,0 +1,42 @@
+class people::danielandrews {
+  # CLI
+  include zsh
+  include ohmyzsh
+  include iterm2::dev
+  include iterm2::colors::solarized_dark
+
+  # Productivity
+  include chrome
+  include dropbox
+  include googledrive
+  include shiftit
+  include flux
+  include alfred
+
+  # Editors
+  include sublime_text
+
+  # OS
+  include osx::finder::show_all_on_desktop
+  include osx::finder::empty_trash_securely
+  include osx::finder::unhide_library
+  include osx::finder::show_hidden_files
+
+  # Support Tim(e) Capsule
+  boxen::osx_defaults { 'Show Unsupported Time Machine Volumes':
+    user    => $::boxen_user,
+    key     => 'TMShowUnsupportedNetworkVolumes',
+    domain  => 'com.apple.systempreferences',
+    value   => true,
+  }
+
+  package { 'htop-osx': }
+  package { 'mtr': }
+  package { 'heroku-toolbelt': }
+
+  file { "$home/.zshrc":
+    content => template('people/hwhelchel/.zshrc.erb')
+  }
+
+  include projects::all
+}
