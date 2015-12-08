92782f1628b0f868b4293c12b9c634944dba3693
hwhelchel <harry.whelchel@salesloft.com>
2015-12-08 10:44:41 -0500

    updates boxen

--- manifests/site.pp
+++ manifests/site.pp
@@ -63,12 +63,16 @@ node default {
     fail('Please enable full disk encryption and try again')
   }
 
+  # node versions
+  nodejs::version { '0.8': }
+  nodejs::version { '0.10': }
+  nodejs::version { '0.12': }
+
   # default ruby versions
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
+  ruby::version { '2.1.7': }
+  ruby::version { '2.2.3': }
 
   # Always provide bundler
   ruby_gem { 'bundler for all rubies':
